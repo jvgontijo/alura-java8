64fd7392d2eea1cee4509a71d7aa88b25f1f91dc
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-17 14:23:32 -0300

    refatorando para lambdas

--- src/io/github/jvgontijo/TestaDefaultMethods.java
+++ src/io/github/jvgontijo/TestaDefaultMethods.java
@@ -1,41 +1,19 @@
 package io.github.jvgontijo;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class TestaDefaultMethods {
 	public static void main(String[] args) {
-		
+
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("é a maior");
 		palavras.add("string");
 		palavras.add("essa");
-		
-		palavras.sort(new Comparator<String>() {
-			@Override
-			public int compare(String s1, String s2) {
-				if(s1.length() > s2.length()) {
-					return 1;
-				}
-				if(s1.length() < s2.length()) {
-					return -1;
-				}
-				return 0;
-			}
-		});
-		
-		Consumer<String> consumidor = new ImprimePalavras();
-		palavras.forEach(consumidor);
-	}
-}
 
-class ImprimePalavras implements Consumer<String>{
+		palavras.sort((String s1, String s2) -> Integer.compare(s1.length(), s2.length()));
 
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
+		palavras.forEach(s -> System.out.println(s));
+		
 	}
-	
 }
