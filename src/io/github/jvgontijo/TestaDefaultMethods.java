4fb43f0f02485bb8989309cd4e52bd7b30ce8282
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-17 12:16:41 -0300

    default methods - utilizando o método sort de uma list

--- src/io/github/jvgontijo/TestaDefaultMethods.java
+++ src/io/github/jvgontijo/TestaDefaultMethods.java
@@ -1,6 +1,7 @@
 package io.github.jvgontijo;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -8,9 +9,22 @@ public class TestaDefaultMethods {
 	public static void main(String[] args) {
 		
 		List<String> palavras = new ArrayList<String>();
-		palavras.add("caçamba");
-		palavras.add("canva");
-		palavras.add("computacional");
+		palavras.add("é a maior");
+		palavras.add("string");
+		palavras.add("essa");
+		
+		palavras.sort(new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				if(s1.length() > s2.length()) {
+					return 1;
+				}
+				if(s1.length() < s2.length()) {
+					return -1;
+				}
+				return 0;
+			}
+		});
 		
 		Consumer<String> consumidor = new ImprimePalavras();
 		palavras.forEach(consumidor);
