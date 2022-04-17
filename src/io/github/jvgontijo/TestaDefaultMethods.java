842d82c6af5d81726387c80e9a00c78651416448
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-17 14:28:11 -0300

    fix envio thread with runnable

--- src/io/github/jvgontijo/TestaDefaultMethods.java
+++ src/io/github/jvgontijo/TestaDefaultMethods.java
@@ -15,13 +15,16 @@ public class TestaDefaultMethods {
 
 		palavras.forEach(s -> System.out.println(s));
 		
-		new Thread(new Runnable() {
-
-		    @Override
-		    public void run() {
-		        System.out.println("Executando um Runnable");
-		    }
-
-		}).start();
+		
+//		new Thread(new Runnable() {
+//
+//		    @Override
+//		    public void run() {
+//		        System.out.println("Executando um Runnable");
+//		    }
+//
+//		}).start();
+		
+		new Thread(() -> System.out.println("Executando um Runnable")).start();
 	}
 }
